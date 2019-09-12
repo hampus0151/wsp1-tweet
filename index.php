34bb2996557aad999ec788f68e4c227c69a76ce7
hampus0151 <hampus.nasvall@gmail.com>
2019-09-12 10:49:25 +0200

    verri good whit working website connected to them database

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Den fantastiska tweetsidan!</h1>
 <?php
 include 'include/dbinfo.php';
 // mysql -u -p
@@ -23,21 +12,11 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-// select * from tweet
 $sth = $dbh->prepare('SELECT * FROM tweet
             JOIN users
             ON tweet.user_id = users.id');
 $sth->execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-//echo "<pre>" . print_r($result,1) . "</pre>";
-foreach ($result as $row) {
-    //echo "<pre>" . print_r($row,1) . "</pre>";
-    echo "<div>";
-    echo "<h2>" . $row['body'] . "</h2>";
-    echo "<p>" . $row['name'] . "</p>";
-    echo "</div>";
-}
+
+include 'views/index_layout.php'
 ?>
-    </div>
-</body>
-</html>
